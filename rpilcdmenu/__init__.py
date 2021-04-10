1a33ce9db2e80fe494503af55695d1711fa0aebf
DascaITA <dscatton@moschino.it>
2021-04-10 07:03:27 -0400

    9

--- rpilcdmenu/__init__.py
+++ rpilcdmenu/__init__.py
@@ -2,7 +2,7 @@ from .rpi_lcd_menu import RpiLCDMenu
 from .rpi_lcd_submenu import RpiLCDSubMenu
 from .rpi_terminal_menu import RpiTerminalMenu
 from .rpi_terminal_submenu import RpiTerminalSubMenu
-from .rpi_i2c_menu imiport RpiI2cMenu
+from .rpi_i2c_menu import RpiI2cMenu
 from .rpi_i2c_submenu import RpiI2cSubMenu
 from .version import __version__
 
