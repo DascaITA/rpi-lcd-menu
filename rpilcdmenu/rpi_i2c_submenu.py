7ddcd09a9bc41cbf53f543bd11bc841c66eae6f3
DascaITA <dscatton@moschino.it>
2021-04-10 06:57:03 -0400

    7

---
+++ rpilcdmenu/rpi_i2c_submenu.py
@@ -0,0 +1,11 @@
+from rpilcdmenu import Rpii2cMenu
+
+
+class RpiI2cSubMenu(RpiI2cMenu):
+    def __init__(self, base_menu):
+        """
+        Initialize SubMenu
+        """
+        self.lcd = base_menu.lcd
+
+        super(RpiI2cMenu, self).__init__(base_menu)
