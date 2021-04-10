c67719059b2fc0d92c7e34c8c32f3f5604d34692
DascaITA <dscatton@moschino.it>
2021-04-10 08:48:46 -0400

    14

--- rpilcdmenu/rpi_i2c_menu.py
+++ rpilcdmenu/rpi_i2c_menu.py
@@ -90,7 +90,7 @@ class RpiI2cMenu(BaseMenu):
             self.message('Menu is empty',0)
             return self
         elif len(self.items) <= 2:
-            self.message((self.current_option == 0 and ">" or " ") + self.items[0].text.ljust(16),0)
+            self.message((self.current_option == 0 and ">" or " ") + self.items[0].text.ljust(16),1)
             if len(self.items) == 2:
                 self.message((self.current_option == 1 and ">" or " ") + self.items[1].ljust(16).text,1)
             return self
